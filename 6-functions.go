017a16bc7bd3f4cec18ae355d365bf945612c5a5
Tomasz Pałucki <tpalucki@gmail.com>
2025-08-20 17:05:40 +0200

    Functions update

--- 6-functions.go
+++ 6-functions.go
@@ -10,6 +10,21 @@ func add(x int, y int) int {
 	return x + y
 }
 
+// you can omit types for params of the same type
+func add2(x, y int) int {
+	return x + y
+}
+
+// you can return different
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println(add(42, 13))
+	fmt.Println(add2(42, 13))
+
+	// btw assignment
+	a, b := swap("world!", "hello")
+	fmt.Println(a, b)
 }
